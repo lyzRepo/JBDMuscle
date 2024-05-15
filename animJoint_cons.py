1b44e8da06d644c4c3bc90cf20514ce369a698ed
Zenz_- <yuanzhen_li@outlook.com>
2024-05-15 04:30:39 -0600

    add twist joint

--- animJoint_cons.py
+++ animJoint_cons.py
@@ -32,32 +32,142 @@ class AnimationJoint(object):
     def create(self, jointName, jointSize, jointScale=1.0):
 
         self.spineJointGrp = []
-        self.pelvic = createJnt("{0}_pelvic".format(jointName), position=[0, (10*jointSize), 0], radius=jointScale)
-        self.back1 = createJnt("{0}_back1".format(jointName), position=[0, (11 * jointSize), 0], radius=jointScale)
-        self.back2 = createJnt("{0}_back2".format(jointName), position=[0, (12 * jointSize), 0], radius=jointScale)
-        self.back3 = createJnt("{0}_back2".format(jointName), position=[0, (13 * jointSize), 0], radius=jointScale)
-        self.neck = createJnt("{0}_neck".format(jointName), position=[0, (14.5 * jointSize), 0], radius=jointScale)
-        self.head = createJnt("{0}_head".format(jointName), position=[0, (15.5 * jointSize), 0], radius=jointScale)
-        self.headEnd = createJnt("{0}_headEnd".format(jointName), position=[0, (16.5 * jointSize), 0], radius=jointScale)
+        self.pelvic = createJnt("{0}_pelvic".format(jointName),
+                                position=[0, (10*jointSize), 0], radius=jointScale)
+        self.back1 = createJnt("{0}_back1".format(jointName),
+                               position=[0, (11 * jointSize), 0], radius=jointScale)
+        self.back2 = createJnt("{0}_back2".format(jointName),
+                               position=[0, (12 * jointSize), 0], radius=jointScale)
+        self.back3 = createJnt("{0}_back2".format(jointName),
+                               position=[0, (13 * jointSize), 0], radius=jointScale)
+        self.neck = createJnt("{0}_neck".format(jointName),
+                              position=[0, (14.5 * jointSize), 0], radius=jointScale)
+        self.head = createJnt("{0}_head".format(jointName),
+                              position=[0, (15.5 * jointSize), (0.2*jointSize)], radius=jointScale)
+        self.headEnd = createJnt("{0}_headEnd".format(jointName),
+                                 position=[0, (16.5 * jointSize), (0.2*jointSize)], radius=jointScale)
         self.spineJointGrp.extend([self.pelvic, self.back1, self.back2, self.back3, self.neck, self.head, self.headEnd])
         [cmds.parent(self.spineJointGrp[i + 1], joint) for i, joint in enumerate(self.spineJointGrp[:-1])]
 
         self.legJointGrp = []
-        self.hip = createJnt("{0}_L_hip".format(jointName), position=[(1*jointSize), (10 * jointSize), 0], radius=jointScale)
-        self.knee = createJnt("{0}_L_knee".format(jointName), position=[(1*jointSize), (5 * jointSize), (0.2*jointSize)], radius=jointScale)
-        self.ankle = createJnt("{0}_L_ankle".format(jointName), position=[(1*jointSize), (1 * jointSize), 0], radius=jointScale)
-        self.ball = createJnt("{0}_L_ball".format(jointName), position=[(1*jointSize), 0, (1*jointSize)], radius=jointScale)
-        self.toe = createJnt("{0}_L_toe".format(jointName), position=[(1*jointSize), 0, (2*jointSize)], radius=jointScale)
+        self.hip = createJnt("{0}_L_hip".format(jointName),
+                             position=[(1*jointSize), (10 * jointSize), 0], radius=jointScale)
+        self.knee = createJnt("{0}_L_knee".format(jointName),
+                              position=[(1*jointSize), (5 * jointSize), (0.2*jointSize)], radius=jointScale)
+        self.ankle = createJnt("{0}_L_ankle".format(jointName),
+                               position=[(1*jointSize), (1 * jointSize), 0], radius=jointScale)
+        self.ball = createJnt("{0}_L_ball".format(jointName),
+                              position=[(1*jointSize), 0, (1*jointSize)], radius=jointScale)
+        self.toe = createJnt("{0}_L_toe".format(jointName),
+                             position=[(1*jointSize), 0, (2*jointSize)], radius=jointScale)
         self.legJointGrp.extend([self.hip,  self.knee, self.ankle, self.ball, self.toe])
         [cmds.parent(self.legJointGrp[i + 1], joint) for i, joint in enumerate(self.legJointGrp[:-1])]
 
         self.armJointGrp = []
-        self.clavicle = createJnt("{0}_L_clavicle".format(jointName), position=[(0.5*jointSize), (14 * jointSize), 0], radius=jointScale)
-        self.shoulder = createJnt("{0}_L_shoulder".format(jointName), position=[(2*jointSize), (14 * jointSize), 0], radius=jointScale)
-        self.elbow = createJnt("{0}_L_elbow".format(jointName), position=[(4.5*jointSize), (14 * jointSize), -(0.2*jointSize)], radius=jointScale)
-        self.wrist = createJnt("{0}_L_wrist".format(jointName), position=[(7.2*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.clavicle = createJnt("{0}_L_clavicle".format(jointName),
+                                  position=[(0.5*jointSize), (14 * jointSize), (0.5*jointSize)], radius=jointScale)
+        self.shoulder = createJnt("{0}_L_shoulder".format(jointName),
+                                  position=[(2*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.elbow = createJnt("{0}_L_elbow".format(jointName),
+                               position=[(4.5*jointSize), (14 * jointSize), -(0.2*jointSize)], radius=jointScale)
+        self.wrist = createJnt("{0}_L_wrist".format(jointName),
+                               position=[(7.2*jointSize), (14 * jointSize), 0], radius=jointScale)
         self.armJointGrp.extend([self.clavicle, self.shoulder, self.elbow, self.wrist])
         [cmds.parent(self.armJointGrp[i + 1], joint) for i, joint in enumerate(self.armJointGrp[:-1])]
+
+        self.thumbJointGrp = []
+        self.thumbMeta = createJnt("{0}_L_thumbMeta".format(jointName),
+                                   position=[(7.6*jointSize), (13.9 * jointSize), (0.5 * jointSize)], radius=jointScale)
+        self.thumb1 = createJnt("{0}_L_thumb1".format(jointName),
+                                position=[(8*jointSize), (13.8 * jointSize), (0.6 * jointSize)], radius=jointScale)
+        self.thumb2 = createJnt("{0}_L_thumb2".format(jointName),
+                                position=[(8.3*jointSize), (13.8 * jointSize), (0.6 * jointSize)], radius=jointScale)
+        self.thumb3 = createJnt("{0}_L_thumb3".format(jointName),
+                                position=[(8.6*jointSize), (13.8 * jointSize), (0.6 * jointSize)], radius=jointScale)
+        self.thumbJointGrp.extend([self.thumbMeta, self.thumb1, self.thumb2, self.thumb3])
+        [cmds.parent(self.thumbJointGrp[i + 1], joint) for i, joint in enumerate(self.thumbJointGrp[:-1])]
+
+        self.indexJointGrp = []
+        self.indexMeta = createJnt("{0}_L_indexMeta".format(jointName),
+                                   position=[(7.5*jointSize), (14 * jointSize), (0.2 * jointSize)], radius=jointScale)
+        self.index1 = createJnt("{0}_L_index1".format(jointName),
+                                position=[(8.3*jointSize), (14 * jointSize), (0.3 * jointSize)], radius=jointScale)
+        self.index2 = createJnt("{0}_L_index2".format(jointName),
+                                position=[(8.7*jointSize), (14 * jointSize), (0.3 * jointSize)], radius=jointScale)
+        self.index3 = createJnt("{0}_L_index3".format(jointName),
+                                position=[(9.0*jointSize), (14 * jointSize), (0.3 * jointSize)], radius=jointScale)
+        self.indexEnd = createJnt("{0}_L_indexEnd".format(jointName),
+                                  position=[(9.3*jointSize), (14 * jointSize), (0.3 * jointSize)], radius=jointScale)
+        self.indexJointGrp.extend([self.indexMeta, self.index1, self.index2, self.index3, self.indexEnd])
+        [cmds.parent(self.indexJointGrp[i + 1], joint) for i, joint in enumerate(self.indexJointGrp[:-1])]
+
+        self.middleJointGrp = []
+        self.middleMeta = createJnt("{0}_L_middleMeta".format(jointName),
+                                    position=[(7.5*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.middle1 = createJnt("{0}_L_middle1".format(jointName),
+                                 position=[(8.3*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.middle2 = createJnt("{0}_L_middle2".format(jointName),
+                                 position=[(8.7*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.middle3 = createJnt("{0}_L_middle3".format(jointName),
+                                 position=[(9.0*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.middleEnd = createJnt("{0}_L_middleEnd".format(jointName),
+                                   position=[(9.3*jointSize), (14 * jointSize), 0], radius=jointScale)
+        self.middleJointGrp.extend([self.middleMeta, self.middle1, self.middle2, self.middle3, self.middleEnd])
+        [cmds.parent(self.middleJointGrp[i + 1], joint) for i, joint in enumerate(self.middleJointGrp[:-1])]
+
+        self.ringJointGrp = []
+        self.ringMeta = createJnt("{0}_L_ringMeta".format(jointName),
+                                  position=[(7.5*jointSize), (14 * jointSize), -(0.2 * jointSize)], radius=jointScale)
+        self.ring1 = createJnt("{0}_L_ring1".format(jointName),
+                               position=[(8.3*jointSize), (14 * jointSize), -(0.3 * jointSize)], radius=jointScale)
+        self.ring2 = createJnt("{0}_L_ring2".format(jointName),
+                               position=[(8.7*jointSize), (14 * jointSize), -(0.3 * jointSize)], radius=jointScale)
+        self.ring3 = createJnt("{0}_L_ring3".format(jointName),
+                               position=[(9.0*jointSize), (14 * jointSize), -(0.3 * jointSize)], radius=jointScale)
+        self.ringEnd = createJnt("{0}_L_ringEnd".format(jointName),
+                                 position=[(9.3*jointSize), (14 * jointSize), -(0.3 * jointSize)], radius=jointScale)
+        self.ringJointGrp.extend([self.ringMeta, self.ring1, self.ring2, self.ring3, self.ringEnd])
+        [cmds.parent(self.ringJointGrp[i + 1], joint) for i, joint in enumerate(self.ringJointGrp[:-1])]
+
+        self.pinkyJointGrp = []
+        self.pinkyMeta = createJnt("{0}_L_pinkyMeta".format(jointName),
+                                   position=[(7.5*jointSize), (14 * jointSize), -(0.4 * jointSize)], radius=jointScale)
+        self.pinky1 = createJnt("{0}_L_pinky1".format(jointName),
+                                position=[(8.1*jointSize), (14 * jointSize), -(0.5 * jointSize)], radius=jointScale)
+        self.pinky2 = createJnt("{0}_L_pinky2".format(jointName),
+                                position=[(8.4*jointSize), (14 * jointSize), -(0.5 * jointSize)], radius=jointScale)
+        self.pinky3 = createJnt("{0}_L_pinky3".format(jointName),
+                                position=[(8.7*jointSize), (14 * jointSize), -(0.5 * jointSize)], radius=jointScale)
+        self.pinkyEnd = createJnt("{0}_L_pinkyEnd".format(jointName),
+                                  position=[(9.0*jointSize), (14 * jointSize), -(0.5 * jointSize)], radius=jointScale)
+        self.pinkyJointGrp.extend([self.pinkyMeta, self.pinky1, self.pinky2, self.pinky3, self.pinkyEnd])
+        [cmds.parent(self.pinkyJointGrp[i + 1], joint) for i, joint in enumerate(self.pinkyJointGrp[:-1])]
+
+        cmds.parent(self.armJointGrp[0], self.spineJointGrp[3])
+        cmds.parent(self.legJointGrp[0], self.spineJointGrp[0])
+        cmds.parent(self.thumbJointGrp[0], self.armJointGrp[-1])
+        cmds.parent(self.indexJointGrp[0], self.armJointGrp[-1])
+        cmds.parent(self.middleJointGrp[0], self.armJointGrp[-1])
+        cmds.parent(self.ringJointGrp[0], self.armJointGrp[-1])
+        cmds.parent(self.pinkyJointGrp[0], self.armJointGrp[-1])
+
+        self.allJoints = cmds.listRelatives(self.spineJointGrp[0], c=True, ad=True)
+        self.visLocalAxis(self.allJoints)
+
+    @undo
+    def visLocalAxis(self, jointList=None, dis=1):
+        if not jointList:
+            jointList = cmds.listRelatives(cmds.ls(sl=True), c=True, ad=True)
+        if jointList:
+            for i in jointList:
+                cmds.setAttr("{0}.displayLocalAxis".format(i), dis)
+
+    @undo
+    def rebulid(self, primaryAxis="y", secondaryAxis="x"):
+        pass
+
+
+
 
 def visPoleVector(jointName, jointList=None):
 
@@ -136,7 +246,9 @@ def visPoleVector(jointName, jointList=None):
     cmds.connectAttr("{0}.worldPosition[0]".format(childLoc), "{0}.controlPoints[2]".format(vpvGeo_shape))
     cmds.connectAttr("{0}.worldPosition[0]".format(midLoc), "{0}.controlPoints[3]".format(vpvGeo_shape))
 
-
+def hidePoleVector(jointName):
+    if cmds.objExists("VPV_{0}".format(jointName)):
+        cmds.delete("VPV_{0}".format(jointName))
 
 
 
---
+++ twist_joints.py
@@ -0,0 +1,116 @@
+import maya.cmds as cmds
+
+def CopyJoint(inputJoint, group=None, name="copy"):
+    cmds.select(clear=True)
+    copyJoint = cmds.joint(n="{0}_{1}".format(group, name), position=(0, 0, 0))
+    cmds.delete(cmds.parentConstraint(inputJoint, copyJoint, mo=False, weight=1))
+    cmds.parent(copyJoint, inputJoint)
+    cmds.makeIdentity(copyJoint, apply=True)
+    return copyJoint
+
+
+def forArmTwist(lowerArm=None, wrist=None, jointCount=3):
+
+    elbowTwistBaseJoint = CopyJoint(lowerArm, group=lowerArm, name="TwistBase")
+    elbowTwistValueJoint = CopyJoint(elbowTwistBaseJoint, group=lowerArm, name="TwistValue")
+    armLength = cmds.getAttr("{0}.ty".format(wrist))
+
+    for i in range(1, jointCount + 1):
+        twistJoints = CopyJoint(lowerArm, group=lowerArm, name="Twist{0}".format(i))
+        cmds.setAttr("{0}.ty".format(twistJoints), armLength / jointCount * i)
+        orCons = cmds.orientConstraint(elbowTwistBaseJoint, elbowTwistValueJoint, twistJoints, mo=False, weight=1)[0]
+        cmds.setAttr("{0}.{1}W0".format(orCons, elbowTwistBaseJoint), 1 / jointCount * (jointCount - i))
+        cmds.setAttr("{0}.{1}W1".format(orCons, elbowTwistValueJoint), 1 / jointCount * i)
+        cmds.setAttr("{0}.interpType".format(orCons), 2)
+
+    cmds.aimConstraint(wrist, elbowTwistValueJoint, aimVector=[0, 1, 0], upVector=[0, 0, 1],
+                       worldUpType="objectrotation", worldUpObject=wrist, worldUpVector=[0, 0, 1])
+
+
+def upperArmTwist(upperArm=None, lowerArm=None, jointCount=3):
+
+    armLength = cmds.getAttr("{0}.ty".format(lowerArm))
+    upperArmCounterTwist = CopyJoint(upperArm, group=upperArm, name="CounterTwist")
+    upperArmTwistUpJoint = CopyJoint(upperArm, group=upperArm, name="Up")
+    cmds.setAttr("{0}.tx".format(upperArmTwistUpJoint), -armLength / 3)
+    clavicleJoint = cmds.listRelatives(upperArm, p=True)[0]
+    cmds.parent(upperArmTwistUpJoint, clavicleJoint)
+
+    upperArmTwistBaseJoint = CopyJoint(upperArmCounterTwist, group=upperArm, name="TwistBase")
+    upperArmTwistValueJoint = CopyJoint(upperArmCounterTwist, group=upperArm, name="TwistValue")
+
+    for i in range(0, jointCount):
+        twistJoints = CopyJoint(upperArm, group=upperArm, name="Twist{0}".format(i + 1))
+        cmds.setAttr("{0}.ty".format(twistJoints), armLength / jointCount * i)
+        orCons = cmds.orientConstraint(upperArmTwistBaseJoint, upperArmTwistValueJoint, twistJoints, mo=False, weight=1)[0]
+        if i == 0:
+            cmds.setAttr("{0}.{1}W0".format(orCons, upperArmTwistBaseJoint), 0.9)
+            cmds.setAttr("{0}.{1}W1".format(orCons, upperArmTwistValueJoint), 0.1)
+        else:
+            cmds.setAttr("{0}.{1}W0".format(orCons, upperArmTwistBaseJoint), 1 / jointCount * (jointCount - i))
+            cmds.setAttr("{0}.{1}W1".format(orCons, upperArmTwistValueJoint), 1 / jointCount * i)
+
+        cmds.setAttr("{0}.interpType".format(orCons), 2)
+
+    cmds.aimConstraint(lowerArm, upperArmCounterTwist, aimVector=[0, 1, 0], upVector=[-1, 0, 0],
+                       worldUpType="object", worldUpObject=upperArmTwistUpJoint)
+    cmds.aimConstraint(lowerArm, upperArmTwistValueJoint, aimVector=[0, 1, 0], upVector=[0, 0, 1],
+                       worldUpType="objectrotation", worldUpObject=upperArm, worldUpVector=[0, 0, 1])
+
+    return upperArmTwistUpJoint
+
+
+def shoulderCounterFilp(upperArm, lowerArm, armUpJoint, rotationAxis="z"):
+    clavicleJo = cmds.listRelatives(upperArm, parent=True)[0]
+
+    jointDn = CopyJoint(upperArm, group=upperArm, name="Dn")
+    lengh = cmds.getAttr("{0}.ty".format(upperArm)) / 2
+    cmds.setAttr("{0}.ty".format(jointDn), lengh)
+
+    cmds.setAttr("{0}.r{1}".format(upperArm, rotationAxis), -90)
+    cmds.parent(jointDn, clavicleJo)
+    cmds.setAttr("{0}.r{1}".format(upperArm, rotationAxis), 0)
+
+    MTMNode = cmds.createNode("multMatrix", name="{0}_MTM".format(upperArm))
+    cmds.connectAttr("{0}.worldMatrix[0]".format(jointDn), "{0}.matrixIn[0]".format(MTMNode))
+    cmds.connectAttr("{0}.worldInverseMatrix[0]".format(upperArm), "{0}.matrixIn[1]".format(MTMNode))
+
+    DCPMNode = cmds.createNode("decomposeMatrix", name="{0}_DCPM".format(upperArm))
+    cmds.connectAttr("{0}.matrixSum".format(MTMNode), "{0}.inputMatrix".format(DCPMNode))
+
+    DotNode = cmds.createNode("vectorProduct", name="{0}_Dot".format(upperArm))
+    cmds.connectAttr("{0}.outputTranslate".format(DCPMNode), "{0}.input1".format(DotNode))
+    cmds.connectAttr("{0}.translate".format(lowerArm), "{0}.input2".format(DotNode))
+    cmds.setAttr("{0}.normalizeOutput".format(DotNode), 1)
+
+    posTempJoint = cmds.duplicate(armUpJoint, name="posTempJoint")[0]
+    cmds.parent(posTempJoint, upperArm)
+
+    for index, axis in enumerate("XYZ"):
+        cmds.setDrivenKeyframe("{0}.translate{1}".format(armUpJoint, axis),
+                               currentDriver="{0}.outputX".format(DotNode))
+
+    cmds.setAttr("{0}.rz".format(upperArm), -90)
+    tempPos = cmds.xform(posTempJoint, translation=True, ws=True, q=True)
+    cmds.xform(armUpJoint, ws=True, translation=(tempPos[0], tempPos[1], tempPos[2]))
+    for index, axis in enumerate("XYZ"):
+        cmds.setDrivenKeyframe("{0}.translate{1}".format(armUpJoint, axis),
+                               currentDriver="{0}.outputX".format(DotNode))
+
+    cmds.setAttr("{0}.rz".format(upperArm), 90)
+    tempPos = cmds.xform(posTempJoint, translation=True, ws=True, q=True)
+    cmds.xform(armUpJoint, ws=True, translation=(tempPos[0], tempPos[1], tempPos[2]))
+    for index, axis in enumerate("XYZ"):
+        cmds.setDrivenKeyframe("{0}.translate{1}".format(armUpJoint, axis),
+                               currentDriver="{0}.outputX".format(DotNode))
+
+    cmds.setAttr("{0}.rz".format(upperArm), 0)
+    cmds.delete(posTempJoint)
+
+
+def autoCreate(upperArm, lowerArm, wrist, rotationAxis="z", jointCount=3):
+    forArmTwist(lowerArm=lowerArm, wrist=wrist, jointCount=jointCount)
+    createArmUpJoint = upperArmTwist(upperArm=upperArm, lowerArm=lowerArm, jointCount=jointCount)
+    shoulderCounterFilp(upperArm=upperArm, lowerArm=lowerArm, armUpJoint=createArmUpJoint, rotationAxis=rotationAxis)
+
+
