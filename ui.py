047fbdaa81d733cecfabce7db73b362604657be0
Lyz <168420012+lyzRepo@users.noreply.github.com>
2024-08-13 19:30:43 -0600

    ui update

--- ui.py
+++ ui.py
@@ -97,6 +97,56 @@ class CollapsibleWidget(QWidget):
         self.setExpanded(not self.headerWdg.isExpanded())
 
 
+class CustomListItem(QWidget):
+    def __init__(self, layout_type, parent=None):
+        super(CustomListItem, self).__init__(parent)
+
+        self.layout_type = layout_type
+        main_layout = QVBoxLayout()
+
+        # 创建一个 QGroupBox 来包含每个布局
+        group_box = QGroupBox(f"{layout_type} Layout Group")
+        layout = QVBoxLayout()
+
+        label = QLabel(f"This is a label inside a {layout_type} layout")
+        layout.addWidget(label)
+
+        if layout_type == "Horizontal":
+            h_layout = QHBoxLayout()
+            h_layout.addWidget(QPushButton("Button 1"))
+            h_layout.addWidget(QPushButton("Button 2"))
+            layout.addLayout(h_layout)
+        elif layout_type == "Grid":
+            h_layout = QHBoxLayout()
+            h_layout.addWidget(QPushButton("Button 1"))
+            h_layout.addWidget(QPushButton("Button 2"))
+            h_layout.addWidget(QPushButton("Button 3"))
+            layout.addLayout(h_layout)
+
+        group_box.setLayout(layout)
+        main_layout.addWidget(group_box)
+        self.setLayout(main_layout)
+
+        # 添加右键菜单
+        self.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.open_menu)
+
+    def open_menu(self, position):
+        menu = QMenu()
+        edit_action = menu.addAction("Edit")
+        build_action = menu.addAction("Build")
+        delete_action = menu.addAction("Delete")
+
+        # 连接每个菜单项到对应的槽函数
+        edit_action.triggered.connect(lambda: self.perform_action("Edit"))
+        build_action.triggered.connect(lambda: self.perform_action("Build"))
+        delete_action.triggered.connect(lambda: self.perform_action("Delete"))
+
+        menu.exec_(self.mapToGlobal(position))
+
+    def perform_action(self, action_name):
+        print(f"{action_name} action triggered for {self.layout_type} layout")
+
 class ForArmTwistTab(QWidget):
     def __init__(self):
         super(ForArmTwistTab, self).__init__()
@@ -189,6 +239,193 @@ class ForArmTwistTab(QWidget):
         pass
 
 
+class UpperArmTwistTab(QWidget):
+    def __init__(self):
+        super(UpperArmTwistTab, self).__init__()
+
+        self.createWidgets()
+        self.createLayout()
+        self.createConnections()
+
+    def createWidgets(self):
+        self.upperArmLe = QLineEdit()
+        self.upperArmLe.setFixedWidth(120)
+        self.lowerArmLe = QLineEdit()
+        self.lowerArmLe.setFixedWidth(120)
+
+        self.validator = QDoubleValidator()
+        self.validator.setRange(-1.0, 1.0)
+        self.validator.setDecimals(3)
+
+        self.AimVecX = QLineEdit()
+        self.AimVecX.setFixedWidth(70)
+        self.AimVecX.setText("0.000")
+        self.AimVecX.setValidator(self.validator)
+
+        self.AimVecY = QLineEdit()
+        self.AimVecY.setFixedWidth(70)
+        self.AimVecY.setText("1.000")
+        self.AimVecY.setValidator(self.validator)
+
+        self.AimVecZ = QLineEdit()
+        self.AimVecZ.setFixedWidth(70)
+        self.AimVecZ.setText("0.000")
+        self.AimVecZ.setValidator(self.validator)
+
+        self.upVecX = QLineEdit()
+        self.upVecX.setFixedWidth(70)
+        self.upVecX.setText("0.000")
+        self.upVecX.setValidator(self.validator)
+
+        self.upVecY = QLineEdit()
+        self.upVecY.setFixedWidth(70)
+        self.upVecY.setText("0.000")
+        self.upVecY.setValidator(self.validator)
+
+        self.upVecZ = QLineEdit()
+        self.upVecZ.setFixedWidth(70)
+        self.upVecZ.setText("1.000")
+        self.upVecZ.setValidator(self.validator)
+
+        self.jntUpAxisX = QLineEdit()
+        self.jntUpAxisX.setFixedWidth(70)
+        self.jntUpAxisX.setText("0.000")
+        self.jntUpAxisX.setValidator(self.validator)
+
+        self.jntUpAxisY = QLineEdit()
+        self.jntUpAxisY.setFixedWidth(70)
+        self.jntUpAxisY.setText("0.000")
+        self.jntUpAxisY.setValidator(self.validator)
+
+        self.jntUpAxisZ = QLineEdit()
+        self.jntUpAxisZ.setFixedWidth(70)
+        self.jntUpAxisZ.setText("1.000")
+        self.jntUpAxisZ.setValidator(self.validator)
+
+        self.upperArmTwistIndexLe = QSpinBox()
+
+        self.apply = QPushButton("Apply")
+
+    def createLayout(self):
+        self.AimVecHbox = QHBoxLayout()
+        self.AimVecHbox.setSpacing(2)
+        self.AimVecHbox.addWidget(self.AimVecX)
+        self.AimVecHbox.addWidget(self.AimVecY)
+        self.AimVecHbox.addWidget(self.AimVecZ)
+
+        self.UpVectorHbox = QHBoxLayout()
+        self.UpVectorHbox.setSpacing(2)
+        self.UpVectorHbox.addWidget(self.upVecX)
+        self.UpVectorHbox.addWidget(self.upVecY)
+        self.UpVectorHbox.addWidget(self.upVecZ)
+
+        self.jntUpAxisHbox = QHBoxLayout()
+        self.jntUpAxisHbox.setSpacing(2)
+        self.jntUpAxisHbox.addWidget(self.jntUpAxisX)
+        self.jntUpAxisHbox.addWidget(self.jntUpAxisY)
+        self.jntUpAxisHbox.addWidget(self.jntUpAxisZ)
+
+        self.applyHbox = QHBoxLayout()
+        self.applyHbox.addStretch()
+        self.applyHbox.addWidget(self.apply)
+
+        self.upArmFormLayout = QFormLayout()
+        self.upArmFormLayout.addRow("Upper Arm Joint:", self.upperArmLe)
+        self.upArmFormLayout.addRow("Lower Arm Joint:", self.lowerArmLe)
+        self.upArmFormLayout.addRow("Joint Axis:", self.AimVecHbox)
+        self.upArmFormLayout.addRow("Up Axis:", self.UpVectorHbox)
+        self.upArmFormLayout.addRow("Joint Up Axis:", self.jntUpAxisHbox)
+        self.upArmFormLayout.addRow("Twist Joint Number:", self.upperArmTwistIndexLe)
+        self.upArmFormLayout.addRow("", self.applyHbox)
+
+        self.GroupBox = QGroupBox()
+        self.GroupBox.setLayout(self.upArmFormLayout)
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.GroupBox)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(self.mainLayout)
+
+    def createConnections(self):
+        pass
+
+
+class ShoulderCounterFilpTab(QWidget):
+    def __init__(self):
+        super(ShoulderCounterFilpTab, self).__init__()
+
+        self.createWidgets()
+        self.createLayout()
+        self.createConnections()
+
+    def createWidgets(self):
+        self.upperArmLe = QLineEdit()
+        self.lowerArmLe = QLineEdit()
+        self.armUpjointLe = QLineEdit()
+        self.lowerArmTag = QLabel("Lower Arm Joint:")
+        self.armUpjointTag = QLabel("Arm Up Joint:")
+
+        self.validator = QDoubleValidator()
+        self.validator.setRange(-1.0, 1.0)
+        self.validator.setDecimals(3)
+
+        self.jointAxisX = QLineEdit()
+        self.jointAxisX.setFixedWidth(70)
+        self.jointAxisX.setText("0.000")
+        self.jointAxisX.setValidator(self.validator)
+
+        self.jointAxisY = QLineEdit()
+        self.jointAxisY.setFixedWidth(70)
+        self.jointAxisY.setText("1.000")
+        self.jointAxisY.setValidator(self.validator)
+
+        self.jointAxisZ = QLineEdit()
+        self.jointAxisZ.setFixedWidth(70)
+        self.jointAxisZ.setText("0.000")
+        self.jointAxisZ.setValidator(self.validator)
+
+        self.rotateAxisCmb = QComboBox()
+        self.rotateAxisCmb.addItems(["x", "y", "z"])
+
+        self.apply = QPushButton("Apply")
+
+    def createLayout(self):
+        self.JointHbox = QHBoxLayout()
+        self.JointHbox.addWidget(self.upperArmLe)
+        self.JointHbox.addWidget(self.lowerArmTag)
+        self.JointHbox.addWidget(self.lowerArmLe)
+
+        self.jointAxisHbox = QHBoxLayout()
+        self.jointAxisHbox.setSpacing(2)
+        self.jointAxisHbox.addWidget(self.jointAxisX)
+        self.jointAxisHbox.addWidget(self.jointAxisY)
+        self.jointAxisHbox.addWidget(self.jointAxisZ)
+
+        self.applyHbox = QHBoxLayout()
+        self.applyHbox.addStretch()
+        self.applyHbox.addWidget(self.apply)
+
+        self.shoulderCounterFilpLayout = QFormLayout()
+        self.shoulderCounterFilpLayout.addRow("Upper Arm Joint:", self.JointHbox)
+        self.shoulderCounterFilpLayout.addRow("Arm Up Joint:", self.armUpjointLe)
+        self.shoulderCounterFilpLayout.addRow("Joint Axis:", self.jointAxisHbox)
+        self.shoulderCounterFilpLayout.addRow("Joint rotation Axis:", self.rotateAxisCmb)
+        self.shoulderCounterFilpLayout.addRow("", self.applyHbox)
+
+        self.GroupBox = QGroupBox()
+        self.GroupBox.setLayout(self.shoulderCounterFilpLayout)
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(self.GroupBox)
+        self.mainLayout.setContentsMargins(0, 0, 0, 0)
+
+        self.setLayout(self.mainLayout)
+
+    def createConnections(self):
+        pass
+
+
 class MainWindow(QDialog):
     def __init__(self, parent=mayaMainWindow()):
         super(MainWindow, self).__init__(parent)
@@ -203,25 +440,33 @@ class MainWindow(QDialog):
         self.forarmWin = ForArmTwistTab()
         self.forarmCollapWdg.addWidget(self.forarmWin)
 
+        self.upperArmCollapWdg = CollapsibleWidget("Upper Arm Twist")
+        self.upperArmWin = UpperArmTwistTab()
+        self.upperArmCollapWdg.addWidget(self.upperArmWin)
+
+        self.shouldCounterFilpCollapWdg = CollapsibleWidget("Shoulder Counter Filp")
+        self.shouldCounterFilpWin = ShoulderCounterFilpTab()
+        self.shouldCounterFilpCollapWdg.addWidget(self.shouldCounterFilpWin)
+
     def createLayout(self):
         self.bodyWdg = QWidget()
 
         self.bodyLayout = QVBoxLayout(self.bodyWdg)
         self.bodyLayout.setContentsMargins(0, 0, 0, 0)
-        self.bodyLayout.setSpacing(3)
+        self.bodyLayout.setSpacing(5)
         self.bodyLayout.setAlignment(Qt.AlignTop)
         self.bodyLayout.addWidget(self.forarmCollapWdg)
+        self.bodyLayout.addWidget(self.upperArmCollapWdg)
+        self.bodyLayout.addWidget(self.shouldCounterFilpCollapWdg)
 
         self.bodyScrollArea = QScrollArea()
-        self.bodyScrollArea.setWidgetResizable(False)
+        self.bodyScrollArea.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.bodyScrollArea.setFrameShape(QFrame.NoFrame)
+        self.bodyScrollArea.setWidgetResizable(True)
         self.bodyScrollArea.setWidget(self.bodyWdg)
 
         mainLayout = QVBoxLayout(self)
-        mainLayout.addWidget(self.bodyWdg)
-
-
-
-
+        mainLayout.addWidget(self.bodyScrollArea)
 
 
 
